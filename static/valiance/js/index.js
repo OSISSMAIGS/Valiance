32df7bd60e72c42b788b5ee9a90746afd2f85e36
Kevin Wilbert Johan <134535621+kyzn-15@users.noreply.github.com>
2025-04-06 23:00:04 +0700

    temporary update

--- static/valiance/js/index.js
+++ static/valiance/js/index.js
@@ -2,3 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const currentYear = new Date().getFullYear();
     document.getElementById("currentYear").innerHTML = currentYear;
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.querySelector('.fixed-icon-container').classList.add('active');
+        
+        // Hilangkan bubble setelah 3 detik
+        setTimeout(function() {
+            document.querySelector('.fixed-icon-container').classList.remove('active');
+        }, 7000);
+    }, 1000);
+});
