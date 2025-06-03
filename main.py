46cbb2d3a27ca915a330856057ae91dfe50500b2
Kevin Wilbert Johan <134535621+kyzn-15@users.noreply.github.com>
2025-06-03 20:22:57 +0700

    update questival

--- main.py
+++ main.py
@@ -60,15 +60,15 @@ def e_questival():
 # LOMBA - Questival
 @app.route("/Events/Questival/Lomba_Basket")
 def lomba_basket():
-    return render_template("valiance/lomba_questival/lomba_basket.html")
+    return render_template("valiance/l_closed.html")
 
 @app.route("/Events/Questival/Lomba_Guess_The_Words")
 def lomba_guess_the_words():
-    return render_template("valiance/lomba_questival/lomba_guess_the_words.html")
+    return render_template("valiance/l_closed.html")
 
 @app.route("/Events/Questival/Lomba_Squid_Game_5v5")
 def lomba_squid_game_5v5():
-    return render_template("valiance/lomba_questival/lomba_squid_game_5v5.html")
+    return render_template("valiance/l_closed.html")
 
 @app.route("/Events/Questival/Lomba_Esport_Valorant")
 def lomba_esport_valorant():
@@ -76,7 +76,7 @@ def lomba_esport_valorant():
 
 @app.route("/Events/Questival/Lomba_Blind_Makeup")
 def lomba_blind_makeup():
-    return render_template("valiance/lomba_questival/lomba_blind_makeup.html")
+    return render_template("valiance/l_closed.html")
 
 @app.route("/Events/Questival/Lomba_Deaf_Blind_Mute_Baking")
 def lomba_deaf_blind_mute_baking():
@@ -84,11 +84,11 @@ def lomba_deaf_blind_mute_baking():
 
 @app.route("/Events/Questival/Lomba_Round_And_Round")
 def lomba_round_and_round():
-    return render_template("valiance/lomba_questival/lomba_round_and_round.html")
+    return render_template("valiance/l_closed.html")
 
 @app.route("/Events/Questival/Lomba_Velocity")
 def lomba_velocity():
-    return render_template("valiance/lomba_questival/lomba_velocity.html")
+    return render_template("valiance/l_closed.html")
 
 # WORKSHOPS - Questival
 @app.route("/Events/Questival/Workshop_Swap_The_Canvas")
@@ -125,9 +125,7 @@ def about_us():
 def page_not_found(error):
     return render_template('/valiance/404.html'), 404
 
-
 # Credence
-
 @app.route('/credence')
 def credence():
     return render_template('valiance/credence.html')
